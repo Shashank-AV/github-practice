3cf56e28ebf4d976672e9fa007d104bfc9c14463
Shashank-AV <shashank.a.v@outlook.com>
2021-02-03 16:32:40 +0530

    logic for chapter program structure excercises

---
+++ program structure/chessBoard.js
@@ -0,0 +1,17 @@
+let size = 8;
+let nextLetter = " ";
+let string = "";
+
+for (let n = 1; n <= size * size; n++) {
+  string += nextLetter;
+  if (n % size == 0) {
+    string += "\n";
+    if (size % 2 == 1) {
+      nextLetter = nextLetter == " " ? "#" : " ";
+    }
+  } else {
+    nextLetter = nextLetter == " " ? "#" : " ";
+  }
+}
+
+console.log(string);
---
+++ program structure/loopTriangle.js
@@ -0,0 +1,7 @@
+let abc = "abc";
+console.log(abc.length);
+
+// Solution 2
+for (let string = "#"; string.length != 7; string += "#") {
+  console.log(string);
+}
